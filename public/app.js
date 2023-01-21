07a18e37360fb81fef28094315c4172fb3d6d28f
Matthew Roberts <matthewjroberts2001@gmail.com>
2023-01-21 16:03:19 -0400

    Even more firebase stuff

--- public/app.js
+++ public/app.js
@@ -1,1 +1,15 @@
-console.log(firebase);
+// Source: https://www.youtube.com/watch?v=q5J5ho7YUhA&t=0s
+
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById("whenSignedIn");
+const whenSignedOut = document.getElementById("whenSignedOut");
+
+const signInButton = document.getElementById("signInButton");
+const signOutButton = document.getElementById("signOutButton");
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInButton.onclick = () => auth.signInWithPopup(provider);
+
+signOutButton.onclick = () => auth.signOut();
