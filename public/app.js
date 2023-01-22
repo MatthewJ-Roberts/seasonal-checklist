09388e09079cbc4efeb3a12461310c4532693bf7
Matthew Roberts <matthewjroberts2001@gmail.com>
2023-01-22 19:54:35 -0400

    Added the bulk of the database code

--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
 // Source: https://www.youtube.com/watch?v=q5J5ho7YUhA&t=0s
 
+// User Auth starts here
+
 const auth = firebase.auth();
 
 const whenSignedIn = document.getElementById("whenSignedIn");
@@ -25,6 +27,61 @@ auth.onAuthStateChanged((user) => {
     } else {
         whenSignedIn.hidden = true;
         whenSignedOut.hidden = false;
+    }
+
+});
+
+// User auth ends here
+
+// Database updating starts here
+
+const database = firebase.firestore();
+
+const addRow = document.getElementById("addRowButton");
+const table = document.getElementById("tableBody");
+
+let databaseRef;
+// Turns off real-time database updates --> Saves cost
+let unsubscribe;
+
+auth.onAuthStateChanged((user) => {
+
+    if (user) {
+        
+        databaseRef = database.collection("anime");
+
+        addRow.onclick = () => {
+
+            const { serverTimestamp } = firebase.firestore.FieldValue;
+
+            databaseRef.add({
+                uid: user.uid,
+                Name: "Bleach",
+                Studio: "Pierrot",
+                Genre: "Action, Adventure, Fantasy",
+                Episodes: "13",
+                MALRating: "9.11",
+                PersonalRating: "8.75",
+                Broadcasts: "Mondays",
+                createdAt: serverTimestamp()
+            });
+
+        }
+
+        unsubscribe = databaseRef
+            .where("uid", "==", user.uid)
+            .onSnapshot(querySnapshot => {
+
+                const items = querySnapshot.docs.map(doc => {
+
+                    
+
+                })
+
+            })
+
+    } else {
+        unsubscribe && unsubscribe();
     }
 
 });
