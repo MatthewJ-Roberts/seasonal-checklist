f0618101586fccfb4fb9034bc3b0e8d748341c77
Matthew Roberts <matthewjroberts2001@gmail.com>
2023-01-22 18:11:35 -0400

    Added some new firebase stuff

--- public/app.js
+++ public/app.js
@@ -8,8 +8,23 @@ const whenSignedOut = document.getElementById("whenSignedOut");
 const signInButton = document.getElementById("signInButton");
 const signOutButton = document.getElementById("signOutButton");
 
+const userName = document.getElementById("dropdownIdUser");
+
 const provider = new firebase.auth.GoogleAuthProvider();
 
 signInButton.onclick = () => auth.signInWithPopup(provider);
 
 signOutButton.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged((user) => {
+
+    if (user) {
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userName.innerHTML = user.displayName;
+    } else {
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+    }
+
+});
